48da6daad38c2afc2819e972fabf197f217b5884
Suyash-Singh14 <suyashsingh1313@gmail.com>
2024-11-07 01:31:30 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+let Name = document.querySelector(".card .name");
+let Bio = document.querySelector(".card .bio");
+let Loc = document.querySelector(".card .location");
+let form = document.querySelector("form");
+let URL_name;
+let button = document.querySelector("button");
+let input = document.querySelector("input");
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+})
+
+button.addEventListener("click", ()=>{
+    URL_name = input.value;    
+    let URL = "https://api.github.com/users/" + URL_name;
+
+    fetch(URL).then((val)=>{
+        return val.json();
+    }).then((data)=>{
+        Name.innerHTML = `${data.name}`;
+        Bio.innerHTML = `${data.bio}`;
+        Loc.innerHTML = `${data.location}`;
+    })
+})
+
+
